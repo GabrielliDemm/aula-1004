0e9011408af233a08a33cd0e19f3e7c2e51b9525
Gabriellidemm <felixgabrielli768@gmail.com>
2025-04-10 16:37:15 -0300

    commit add ui

--- Ola.java
+++ Ola.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class Ola{
     public static void main(String[] args){
         System.out.println("boa tarde a todos!");
+        JOptionPane.showMessageDialog(null, "boa tarde a todos");
     }
 }
